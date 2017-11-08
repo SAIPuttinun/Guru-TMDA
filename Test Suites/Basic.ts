afdd6ab7402a4c9f646bdfe1ca92d5c673273683
PIEW-PC\PIEW <piew_teerapong@hotmail.co.th>
2017-11-08 17:44:28 +0700

    1231245

---
+++ Test Suites/Basic.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-11-08T17:22:29</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>90ae1a7d-9c80-47d2-8877-7001fe388b43</testSuiteGuid>
+   <testCaseLink>
+      <guid>074a12d2-d77f-46a9-b996-bd847122c27d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1. Start Page (without login)/Single Verbatim without login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>29ee8125-b6fc-49a2-97be-5852e98edab4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2. Login/Sign Up</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4ec9997e-0736-4f3e-b9c0-fe2fbbfb2dea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2. Login/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>30e39f5f-63fb-4dbf-98c7-2b1fef47d7f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3. Menu Text Prediction/Single Verbatim with login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ed25f24-39fd-450a-a985-506b1874c00c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3. Menu Text Prediction/Upload File without labelling</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f7db2d06-eaf7-4bf0-b841-788f065f7612</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3. Menu Text Prediction/Upload File with labelling</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a022ceea-856f-4d48-ba69-2d426a17c2c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3. Menu Text Prediction/Multiple Verbatim Predicted Result (Predicted in detail)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>12e623d6-f6d1-4c68-aec7-cdecea64eaac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Search user list</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33e6c178-cbde-4152-af20-a3430cbcd675</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Change role and status</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2cb5ab38-e6ac-4311-89bc-7de3c4971068</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Edit profile management</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1660012f-ed92-49cd-85a5-a00704ff3888</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Change user profile picture</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5449d816-0084-445e-a654-bb8fb1bfdc41</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Reset and Change password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>230a553e-e433-4bfd-9425-459f7ce0bae2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Menu User Management/Edit user profile</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8665395b-c00f-4243-bd25-0731d7fd851e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/5. History Log/Search history log</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
